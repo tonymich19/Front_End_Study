5ee1d399facf0a739bdb9ab094dd985696245de8
tony correia <tony.correia19@outlook.com>
2017-05-24 00:51:27 -0300

    Este é um pequeno projeto de uma pagina estatica responsiva. Pagina simples , fazendo uso de HTML, CSS, Javascript.

---
+++ hotelbv.js
@@ -0,0 +1,10 @@
+	function horaAtual() {
+    var d = alert(new Date());
+		var f = new Date();
+    document.getElementById("demo").innerHTML = f;
+	}
+	function finalizar() {
+    var e = alert ("Reserva RSV-HBV-001 Realizada com Sucesso! ")
+		document.getElementById("fim").innerHTML = e;
+
+	}
